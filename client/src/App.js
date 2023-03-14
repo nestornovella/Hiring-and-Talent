9d8c941574395d9baa6ef71910b58fa31b765a86
Eduardo Toro <97927299+Eduardo9324@users.noreply.github.com>
2023-03-14 16:10:11 -0500

    front one

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import Styles from './styles/app.module.css'
 import logo from './img/hiring-and-talent.jpg'
 import equip from './img/equip-working.png'
 
-function App() {
+const App = () => {
   return (
     <div className={Styles.container}>
-     
       <img width={200} src={logo} alt='logo' />
-      <img width={500} src={equip} alt={"equip"}/>
+      <img width={500} src={equip} alt={"equip"} />
     </div>
   );
-}
+};
 
 export default App;
